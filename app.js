c08cbcfcede867e8fc271f4285c365eeaae7a93b
newwave8 <new.wave.hatch@gmail.com>
2018-04-30 13:22:11 +0000

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 'use strict';
+const memo = new Map(); // 連想配列Mapを使って計算結果を保存（メモ化）する
+memo.set(0, 0); // keyには順番、valueには答えを格納する
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if (memo.has(n)) { // 連想配列が、nをキーとした答えを持っていればその値をそのまま関数の値として返す
+        return memo.get(n);
     }
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n-1) + fib(n-2); // そうでない場合は、再帰関数を計算して値を求め、それを連想配列に格納した後に返す
+    memo.set(n, value);
+    return value;
 }
 
 const length = 40;
